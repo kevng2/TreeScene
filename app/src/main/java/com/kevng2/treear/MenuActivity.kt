c9be36c83d5bf6a67153427fd01ebecf8c5eb2a4
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-09-06 13:44:43 -0400

    added Spinner dropdown menu

--- app/src/main/java/com/kevng2/treear/MenuActivity.kt
+++ app/src/main/java/com/kevng2/treear/MenuActivity.kt
@@ -3,7 +3,10 @@ package com.kevng2.treear
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bumptech.glide.Glide
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
@@ -11,13 +14,29 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
-        Glide.with(this)
-            .load(R.drawable.tree)
-            .into(background_image)
-
         start_button.setOnClickListener {
             val intent = Intent(this@MenuActivity, CameraActivity::class.java)
             startActivity(intent)
+        }
+
+        val treeList = arrayListOf("Oak", "Elm", "Pine", "Palm")
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+            treeList)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        tree_spinner.adapter = arrayAdapter
+        tree_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@MenuActivity, treeList[position], Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
