c80fc4cb61a11b5b6df84ef0f2ede80cd359fa6a
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-09-07 15:06:35 -0400

    implemented material menu bar and removed spinner from project

--- app/src/main/java/com/kevng2/treear/CameraActivity.kt
+++ app/src/main/java/com/kevng2/treear/CameraActivity.kt
@@ -2,11 +2,9 @@ package com.kevng2.treear
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
@@ -19,37 +17,38 @@ import kotlinx.android.synthetic.main.activity_camera.*
 class CameraActivity : AppCompatActivity() {
     private var mArFragment: ArFragment? = null
     private var mModelRenderable: ModelRenderable? = null
-    private var mModelId: Int = 0
+    private var mModelId: Int = R.raw.oak_tree
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+        setSupportActionBar(camera_toolbar)
         setModel(mModelId)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
         mArFragment = supportFragmentManager.findFragmentById(R.id.fragment) as ArFragment
         setUpPlane()
+    }
 
-        val treeList = arrayListOf("Oak", "Pine", "Elm", "Palm", "Cherry Blossom")
-        val arrayAdapter = ArrayAdapter<String>(
-            this, android.R.layout.simple_spinner_item,
-            treeList
-        )
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        tree_selection_spinner.adapter = arrayAdapter
-        tree_selection_spinner.onItemSelectedListener =
-            object : AdapterView.OnItemSelectedListener {
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
-                }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.activity_camera, menu)
+        return true
+    }
 
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    setModel(position)
-                }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        if (isModel(item.itemId)) {
+            when (item.itemId) {
+                R.id.oak -> mModelId = R.raw.oak_tree
+                R.id.pine -> mModelId = R.raw.model
+                R.id.elm -> mModelId = R.raw.elm_tree
+                R.id.palm -> mModelId = R.raw.queen_palm_tree
+                R.id.cherry_blossom -> mModelId = R.raw.cherry_blossom
             }
+            setUpModel()
+            Log.d("CameraActivity", item.itemId.toString())
+        }
+        return true
     }
 
     private fun setUpPlane() {
@@ -92,5 +91,14 @@ class CameraActivity : AppCompatActivity() {
             4 -> mModelId = R.raw.cherry_blossom
         }
         setUpModel()
+    }
+
+    private fun isModel(id: Int): Boolean {
+        if (id == R.id.oak || id == R.id.pine || id == R.id.elm ||
+            id == R.id.palm || id == R.id.cherry_blossom
+        ) {
+            return true
+        }
+        return false
     }
 }
--- app/src/main/java/com/kevng2/treear/MenuActivity.kt
+++ app/src/main/java/com/kevng2/treear/MenuActivity.kt
@@ -3,6 +3,7 @@ package com.kevng2.treear
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
@@ -12,10 +13,16 @@ class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-
+        setSupportActionBar(menu_toolbar)
         val intent = Intent(this, CameraActivity::class.java)
         start_button.setOnClickListener {
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.activity_menu, menu)
+        return true
     }
 }
