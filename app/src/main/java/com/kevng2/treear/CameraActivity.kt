383b052b84cc58480ced4be7266f1704e624494c
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-09-10 17:48:29 -0400

    deleted import statements

--- app/src/main/java/com/kevng2/treear/CameraActivity.kt
+++ app/src/main/java/com/kevng2/treear/CameraActivity.kt
@@ -1,6 +1,5 @@
 package com.kevng2.treear
 
-import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,8 +7,6 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.snackbar.Snackbar
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.NodeParent
