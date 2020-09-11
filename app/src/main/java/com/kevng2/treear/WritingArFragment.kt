5cc13de404b644c3d0bd62d73b2b30f3ce3dfa8b
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-09-11 18:21:38 -0400

    Created class that promps user for consent when the application starts

---
+++ app/src/main/java/com/kevng2/treear/WritingArFragment.kt
@@ -0,0 +1,16 @@
+package com.kevng2.treear
+
+import com.google.ar.sceneform.ux.ArFragment
+
+class WritingArFragment : ArFragment() {
+    override fun getAdditionalPermissions(): Array<String?> {
+        val additionalPermissions: Array<String> = super.getAdditionalPermissions()
+        val permissionLength = additionalPermissions.size
+        val permission = arrayOfNulls<String>(permissionLength + 1)
+        permission[0] = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        if (permissionLength > 0) {
+            System.arraycopy(additionalPermissions, 0, permission, 1, additionalPermissions.size)
+        }
+        return permission
+    }
+}
