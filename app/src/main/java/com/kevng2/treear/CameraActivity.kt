c67c67200a90a1dc925dcf31157d44ff68d524fa
Kevin Nguyen <nguyenkt6123@gmail.com>
2020-09-11 18:22:08 -0400

    Added a floating action to let the user take pictures
    generateFilename() generates a filename for a picture depending on the
    date and the directory of the user's pictures
    
    saveBitmapToDisk() saves it to user's disk
    
    takePhoto() creates a Snackbar that displays "Photo saved" and gives the
    user the option to go and view the picture in their default pictures
    app""

--- app/src/main/java/com/kevng2/treear/CameraActivity.kt
+++ app/src/main/java/com/kevng2/treear/CameraActivity.kt
@@ -1,12 +1,20 @@
 package com.kevng2.treear
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Handler
+import android.os.HandlerThread
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.PixelCopy
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
+import com.google.android.material.snackbar.Snackbar
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.NodeParent
@@ -14,6 +22,11 @@ import com.google.ar.sceneform.rendering.ModelRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_camera.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraActivity : AppCompatActivity() {
     private var mArFragment: ArFragment? = null
@@ -29,6 +42,10 @@ class CameraActivity : AppCompatActivity() {
         supportActionBar?.setDisplayShowTitleEnabled(false)
         setUpModel()
         setUpPlane()
+
+        take_picture_button.setOnClickListener {
+            takePhoto()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -54,8 +71,7 @@ class CameraActivity : AppCompatActivity() {
             R.id.trash -> {
                 if (isInsertMode) {
                     item.setIcon(R.drawable.ic_done)
-                }
-                else {
+                } else {
                     item.setIcon(R.drawable.ic_delete)
                 }
 
@@ -65,15 +81,17 @@ class CameraActivity : AppCompatActivity() {
                 Log.d("CameraActivity", "onOptionsItemSelected (line 68): ")
                 AlertDialog.Builder(this@CameraActivity)
                     .setPositiveButton(android.R.string.ok) { dialog, which -> }
-                    .setMessage("Welcome to TreeVisualizer\n\n" +
-                            "To start, wave your camera around like the animation is indicating on " +
-                            "the camera preview. Then, an indicator will pop up to plant some trees!\n\n" +
-                            "Tap on any dotted area to plant a tree\n\n" +
-                            "Add Icon: Switch tree type\n\n" +
-                            "Trash Icon: Delete mode. Tap on tree to delete\n\n" +
-                            "Check mark Icon: Switch back to add mode\n\n" +
-                            "Pinch to grow the tree\n\n" +
-                            "Hold one finger on the tree and use another finger to reorient the tree\n\n")
+                    .setMessage(
+                        "Welcome to TreeVisualizer\n\n" +
+                                "To start, wave your camera around like the animation is indicating on " +
+                                "the camera preview. Then, an indicator will pop up to plant some trees!\n\n" +
+                                "Tap on any dotted area to plant a tree\n\n" +
+                                "Add Icon: Switch tree type\n\n" +
+                                "Trash Icon: Delete mode. Tap on tree to delete\n\n" +
+                                "Check mark Icon: Switch back to add mode\n\n" +
+                                "Pinch to grow the tree\n\n" +
+                                "Hold one finger on the tree and use another finger to reorient the tree\n\n"
+                    )
                     .create()
                     .show()
             }
@@ -127,5 +145,60 @@ class CameraActivity : AppCompatActivity() {
             return true
         }
         return false
+    }
+
+    private fun generateFilename(): String {
+        val date: String = SimpleDateFormat("yyyyMMddHHmmss", Locale.getDefault()).format(
+            Date()
+        )
+        return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            .toString() + File.separator + "Sceneform/" + date + "_screenshot.jpg"
+    }
+
+    private fun saveBitmapToDisk(bitmap: Bitmap, filename: String) {
+        val out = File(filename)
+        if (!out.parentFile?.exists()!!) {
+            out.parentFile?.mkdirs()
+        }
+        val outputStream = FileOutputStream(filename)
+        val outputData = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputData)
+        outputData.writeTo(outputStream)
+        outputStream.flush()
+        outputStream.close()
+    }
+
+    private fun takePhoto() {
+        val filename = generateFilename()
+        val frag = fragment as ArFragment
+        val view = frag.arSceneView
+        val bitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+
+        val handlerThread = HandlerThread("PixelCopier")
+        handlerThread.start()
+
+        PixelCopy.request(view, bitmap, { copyResult ->
+            if (copyResult == PixelCopy.SUCCESS) {
+                saveBitmapToDisk(bitmap, filename)
+                val snackbar = Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "Photo saved",
+                    Snackbar.LENGTH_LONG
+                )
+                snackbar.setAction("Open in Photos") { v ->
+                    val photoFile = File(filename)
+                    val photoUri = FileProvider.getUriForFile(
+                        this@CameraActivity,
+                        "$packageName.ar.codelab.name.provider", photoFile
+                    )
+                    intent = Intent(Intent.ACTION_VIEW, photoUri)
+                    intent.setDataAndType(photoUri, "image/*")
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    startActivity(intent)
+                }
+                snackbar.show()
+            }
+            handlerThread.quitSafely()
+        }, Handler(handlerThread.looper))
     }
 }
